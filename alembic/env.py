dea7f54167e66ef113548715d1231289c2947bf1
Alin Mihaila <alin.mh996@gmail.com>
2021-05-08 18:34:44 +0300

    test migrations

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from imable_backend.database.session import Base
+from src.imable_backend.database.session import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
